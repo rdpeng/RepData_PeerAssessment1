f736383e743a6a5464976f17e0094c5c5c96d4c0
slyone2 <cashsylvester@yahoo.com>
2016-03-06 23:12:22 -0500

    final draft

--- untitled.R
+++
@@ -1,3 +0,0 @@
-library(knitr)
-setwd("C:/Users/scash/RepData_PeerAssessment1/assignment1/RepData_PeerAssessment1")
-knit2html("C:/Users/scash/RepData_PeerAssessment1/assignment1/RepData_PeerAssessment1/PA1_template.Rmd")
