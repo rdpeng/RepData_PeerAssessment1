ccbff5286b92e01b03f068085ad5033cc2b6df12
slyone2 <cashsylvester@yahoo.com>
2016-03-06 22:19:57 -0500

    updated files

---
+++ untitled.R
@@ -0,0 +1,3 @@
+library(knitr)
+setwd("C:/Users/scash/RepData_PeerAssessment1/assignment1/RepData_PeerAssessment1")
+knit2html("C:/Users/scash/RepData_PeerAssessment1/assignment1/RepData_PeerAssessment1/PA1_template.Rmd")
