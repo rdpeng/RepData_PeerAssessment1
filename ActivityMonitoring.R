5f9e655e008d4b28be169039ce69592e2182da90
Fbarangan <franb86.2@gmail.com>
2016-04-19 23:47:04 -0400

    initial commit

--- ActivityMonitoring.R
+++ ActivityMonitoring.R
@@ -1,1 +1,20 @@
 # Course Assingment 1
+library(dplyr)
+
+#load csv file
+activity <- read.csv( "activity.csv")
+activity$Date <- as.POSIXct(activity$date)
+
+
+# check null/NA values.
+stepsNull <- sum(is.na(activity$steps))
+dateNull <- sum(is.na(activity$date))
+
+
+#convert csv to dplyr (tbl.df)
+activity <- tbl_df(activity)
+activity <- activity %>%
+        filter(!is.na(steps)) %>%
+        select(steps, Date, interval)
+
+
